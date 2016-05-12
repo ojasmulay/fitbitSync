6b7e77527d2c9597fd03b4a0583217add09720f8
ojasmulay <ojasmulay@gmail.com>
2016-05-12 11:34:16 -0700

    merging form branch

--- FitBit Sync 1.0/FitBit Sync 1.0/SupportViewController.swift
+++ FitBit Sync 1.0/FitBit Sync 1.0/SupportViewController.swift
@@ -12,6 +12,8 @@ class SupportViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
     
