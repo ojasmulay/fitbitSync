43bae910c33f175a40590820ae2e08ed6ea6f862
ojasmulay <ojasmulay@gmail.com>
2016-05-12 11:28:19 -0700

    commit to branch

--- FitBit Sync 1.0/FitBit Sync 1.0/SupportViewController.swift
+++ FitBit Sync 1.0/FitBit Sync 1.0/SupportViewController.swift
@@ -7,12 +7,15 @@
 //
 
 import UIKit;
+import HealthKit;
 
 class SupportViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
     
     override func didReceiveMemoryWarning() {
